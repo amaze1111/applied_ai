c36b58179b7adb33489f9fc7350df49e1a608653
Ashish <prince.ashish11@gmail.com>
2025-05-23 07:05:16 +0530

    Added a scheduler

--- new_apps/main.py
+++ new_apps/main.py
@@ -4,10 +4,18 @@ from summarizer import summarizer
 from telegram_sender import send_telegram_message
 
 def main():
-    topic = get_user_input()
+    # topic = get_user_input()  
+    # Skip user input for scheduler
+    topic = "Bengaluru"
     news_results = fetch_news(topic)
     summary = summarizer(news_results)
     send_telegram_message(summary)
+
+def scheduled_job():
+    news = fetch_news()
+    summary = summarizer(news)
+    send_telegram_message(summary)
+
 
 if __name__ == "__main__":
     main()
